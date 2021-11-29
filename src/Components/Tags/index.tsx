5d195d44ad3afc58ccbe02abc53640c91f7d7d62
Zahid Ronty <230759@usn.no>
2021-11-29 06:23:39 +0100

    solving color parameter

--- src/Components/Tags/index.tsx
+++ src/Components/Tags/index.tsx
@@ -1,3 +1,5 @@
+import { useEffect } from "react";
+
 interface dataProps {
   color: string;
   name: string;
@@ -7,6 +9,7 @@ const Tags: React.FC<dataProps> = ({ color, name }) => {
   const styles = {
     body: `bg-${color}-100 text-${color}-500 ring-2 ring-${color}-300 rounded-md w-36 text-center py-1 text-xs font-bold font-sans`,
   };
+  useEffect(() => {}, [color, name]);
   return (
     <div className={styles.body}>
       <p>{name}</p>
