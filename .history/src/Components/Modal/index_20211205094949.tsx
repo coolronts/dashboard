1e322c4632d44258ffaa7f1d5cd36572bda1861a
Zahid Ronty <230759@usn.no>
2021-12-05 10:12:34 +0100

    Modal Fixed

--- .history/src/Components/Modal/index_20211205094949.tsx
+++ .history/src/Components/Modal/index_20211205094949.tsx
@@ -8,7 +8,8 @@ const Modal: React.FC<ModalProps> = ({ children, handleModal }) => {
   const styles = {
     background:
       "fixed w-full h-full overflow-auto bg-gray-900 opacity-95 top-0 left-0 z-10",
-    body: "absolute bg-white top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 p-24 rounded-2xl shadow-2xl",
+    container: "relative h-full h-full bg-transparent opacity-100",
+    body: "bg-white absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 p-24 rounded-2xl shadow-2xl opacity-100 ",
     close:
       "absolute top-0 right-0 m-4 cursor-pointer text-2xl text-gray-500 hover:text-gray-700",
   };
@@ -16,11 +17,13 @@ const Modal: React.FC<ModalProps> = ({ children, handleModal }) => {
   return (
     <>
       <div className={styles.background}>
-        <div className={styles.body}>
-          <div className={styles.close} onClick={() => handleModal(false)}>
-            <Cancel />
+        <div className={styles.container}>
+          <div className={styles.body}>
+            <div className={styles.close} onClick={() => handleModal(false)}>
+              <Cancel />
+            </div>
+            {children}
           </div>
-          {children}
         </div>
       </div>
     </>
